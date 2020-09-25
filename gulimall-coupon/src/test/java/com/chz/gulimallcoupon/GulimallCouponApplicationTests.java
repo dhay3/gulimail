f93ea0b8625b50d303db9f619c68452e6b2ab7bf
cyberPelican <hostlockdown@gmail.com>
2020-09-25 10:37:34 +0800

    项目构建

---
+++ gulimall-coupon/src/test/java/com/chz/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chz.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
